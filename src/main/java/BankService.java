40adcff13a7261d655645fdf8701bbcac8062b80
genead <jirabona990@gmail.com>
2023-07-25 04:40:57 +0000

    done

--- src/main/java/BankService.java
+++ src/main/java/BankService.java
@@ -23,7 +23,7 @@ public class BankService {
      * @param amount the amount to be deposited.
      */
     public void deposit(double amount){
-
+       System.out.println(this.balance += amount);
     }
 
     /**
@@ -32,7 +32,11 @@ public class BankService {
      * @param amount the amount to be withdrawn.
      */
     public void withdraw(double amount){
-
+        if((this.balance - amount) < 0){
+            System.out.println("you don't enough balance!!");
+        }else{
+            System.out.println(this.balance -= amount);
+        }
     }
 
     /**
@@ -40,6 +44,6 @@ public class BankService {
      * @return the user's balance.
      */
     public double getBalance(){
-        return 0;
+        return (this.balance);
     }
 }
